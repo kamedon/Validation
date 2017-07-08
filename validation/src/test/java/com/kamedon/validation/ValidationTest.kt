22d46c9ffcda8a4b58b4f2ec3cfa91117a4e04b0
kamedon <hkamei@gmail.com>
2017-07-08 17:52:25 +0900

    modify validation syntax

--- validation/src/test/java/com/kamedon/validation/ValidationTest.kt
+++ validation/src/test/java/com/kamedon/validation/ValidationTest.kt
@@ -6,51 +6,27 @@ import org.junit.Test
 /**
  * Created by Kamedon
  */
+class User(val name: String, val age: Int, val validation: Validation<User>) {
+    fun validate() = validation.validate(this)
+}
+
 class ValidationTest {
 
     @Test
     fun validate_1Test() {
         val validation = Validation<User> {
-            be { name.length >= 5 } not "name: 5 characters or more"
-            be { age >= 20 } not "age: Over 20 years old"
+            "name"{
+                be { name.length >= 5 } not "name: 5 characters or more"
+            }
+            "age"{
+                be { age >= 20 } not "age: Over 20 years old"
+                be { age >= 30 } not "age: Over 20 years old"
+            }
         }
-        User("kamedon", 30, validation).validate().size.should.be(0)
-        User("", 20, validation).validate().size.should.be(1)
-        User("", 0, validation).validate().size.should.be(2)
-    }
-
-    @Test
-    fun validate_2Test() {
-        val validation = Validation<User> {
-            of { name } be { length >= 5 } not "name: 5 characters or more"
-            of { age } be { this >= 20 } not "age: Over 20 years old"
-        }
-        User("kamedon", 30, validation).validate().size.should.be(0)
-        User("", 20, validation).validate().size.should.be(1)
-        User("", 0, validation).validate().size.should.be(2)
-    }
-
-    @Test
-    fun validate_3Test() {
-        User2("kamedon", 30).validate().size.should.be(0)
-        User2("", 20).validate().size.should.be(1)
-        User2("", 0).validate().size.should.be(2)
+        print(User("kamedon", 30, validation).validate())
+        print(User("", 0, validation).validate())
+        val errors = User("kamedon", 30, validation)
+//        User("", 20, validation).validate().size.should.be(1)
+//        User("", 0, validation).validate().size.should.be(2)
     }
 }
-
-
-class User(val name: String, val age: Int, val validation: Validation<User>) {
-    fun validate() = validation.validate(this)
-}
-
-class User2(val name: String, val age: Int) {
-
-    val validation = Validation<User2> {
-        of { name } be { length >= 5 } not "name: 5 characters or more"
-        of { age } be { this >= 20 } not "age: Over 20 years old"
-    }
-
-    fun validate() = validation.validate(this)
-}
-
-
