9d03d66d1c4d3dcdf178d0c837ca49d1ab69a2ee
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2018-08-13 15:55:31 +0900

    add test testcase

--- validation/src/test/java/com/kamedon/validation/ValidationsTest.kt
+++ validation/src/test/java/com/kamedon/validation/ValidationsTest.kt
@@ -45,8 +45,62 @@ class ValidationsTest {
         val user = User("kamedon", 30)
         val validation = Validations.get<User>()
         Assert.assertEquals(true, validation != null)
-        val valid = (validation as Validation<User>).validate(user)
+        val valid = validation?.validate(user) ?: return Assert.fail("validation not found")
         Assert.assertEquals(0, valid.size)
     }
+
+    @Test
+    fun invalidDataText() {
+        Validations {
+            define<User> {
+                "name"{
+                    be { name.length >= 5 } not "name: 5 characters or more"
+                    be { name.length <= 10 } not "name: 10 characters or less"
+                }
+                "age"{
+                    be { age >= 20 } not "age: Over 20 years old"
+                }
+            }
+        }
+
+        val user = User("kamedon", 0)
+        val validation = Validations.get<User>()
+        Assert.assertEquals(true, validation != null)
+        val valid = validation?.validate(user) ?: return Assert.fail("validation not found")
+        Assert.assertEquals(1, valid.size)
+    }
+
+    @Test
+    fun multiTest() {
+        Validations {
+            define<User> {
+                "name"{
+                    be { name.length >= 5 } not "name: 5 characters or more"
+                    be { name.length <= 10 } not "name: 10 characters or less"
+                }
+                "age"{
+                    be { age >= 20 } not "age: Over 20 years old"
+                }
+            }
+            define<Account> {
+                "name"{
+                    be { name.isNotBlank() } not "name is not blank"
+                }
+
+            }
+        }
+
+        val user = User("kamedon", 0)
+        val validation = Validations.get<User>()
+        val valid = validation?.validate(user) ?: return Assert.fail("validation not found")
+        Assert.assertEquals(1, valid.size)
+
+        val account = Account("")
+        val validationAccount = Validations.get<Account>()
+        val validAccount = validationAccount?.validate(account)
+                ?: return Assert.fail("validation not found")
+        Assert.assertEquals(1, validAccount.size)
+    }
+
 
 }
